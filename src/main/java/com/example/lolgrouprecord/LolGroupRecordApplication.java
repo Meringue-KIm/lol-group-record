f32d57ab6f1299e70316c224ef6715e953dc9eed
Meringue-KIm <dd8873@naver.com>
2024-01-12 20:35:11 +0900

    test commit

---
+++ src/main/java/com/example/lolgrouprecord/LolGroupRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.lolgrouprecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolGroupRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolGroupRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lolgrouprecord/LolGroupRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lolgrouprecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolGroupRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
